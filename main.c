117e685d0541632652be7f51566969f6e5064232
yukuri611 <yukiiw611@gmail.com>
2025-05-31 09:52:56 +0900

    足し算引き算を実装

--- main.c
+++ main.c
@@ -2,16 +2,35 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <number>\n", argv[0]);
-        return 1;
+  if (argc != 2) {
+    fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
+    return 1;
+  }
+
+  char *p = argv[1];
+
+  printf(".intel_syntax noprefix\n");
+  printf(".global main\n");
+  printf("main:\n");
+  printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add rax, %ld\n", strtol(p, &p, 10));
+      continue;
     }
 
-    printf(".intel_syntax noprefix\n");
-    printf(".globl main\n");
-    printf("main:\n");
-    printf("    mov rax, %d\n", atoi(argv[1]));
-    printf("    ret\n");
-    printf(".section .note.GNU-stack,\"\",@progbits\n");
-    return 0;
+    if (*p == '-') {
+      p++;
+      printf("  sub rax, %ld\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "unexpected character: '%c'\n", *p);
+    return 1;
+  }
+
+  printf("  ret\n");
+  return 0;
 }
